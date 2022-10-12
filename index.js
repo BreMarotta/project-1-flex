5d844781f7a945b8d51a07534ed760e5d0ba888d
Breanne <BreMarotta@gmail.com>
2022-10-12 14:44:30 -0400

    worked with student again, this is an update of current code. LOTS of issues.

--- index.js
+++ index.js
@@ -1,23 +1,24 @@
-/**
+
+/** 
  *how to think of events(3 question rule)* 
  *at what time can i do this events.
  *whats going to trigger event(click)
  *Effect (display and homepage)
  /**globals **/
- document.addEventListener("domContentLoaded"), () => {
-  RenderListHomepage();
+ document.addEventListener("DOMContentLoaded"), () => {
+  renderListHomePage();
   }
 // NODE GETTERS
-const mainDiv = () => document.getElementById("main");
-const homePageLink = document.getElementById (homePageLink)
+const mainDiv = document.getElementById("main");
+
 /**templates **/
 const homepagesTemplate = () => {
   return
-     <h1 class="centre-align">welcome to our cat tracker</h1>
+     <h1 class="centre-align"> cat tracker app</h1>
 } 
 /**renders**/
-const rendersHomePage = () => {
-  mainDiv().innerHTML =homepagesTemplate();
+const renderListHomePage = () => {
+  mainDiv.innerHTML =homepagesTemplate();
 }
 // eventListeners
 function AttachListCatClickEvent() {
@@ -31,35 +32,47 @@ function attachHomepageClickEvent() {
 //eventHandlers
 function RenderHomepage()
   //create the Html elements for the homepage
+  //post request to render cat magesContainer
   //reset mainDiv();
  
-  h1.InnerText ="cat list page"
-  h1.StyleMarginTop ="0"
-  base_url = 'https://api.thecatapi.com/v1/images/search?limit=10'
+h1.InnerText ="cat list page"
+h1.StyleMarginTop ="0"
+base_url = 'https://api.thecatapi.com/v1/images/search?limit=10'
   document.addEventListener("DOMContentLoaded", () => {
     renderCatImages();
     
     function renderCatImages(){
       fetch (base_url)
-      .then(res => res.json())
-    .then(data => {
-      console.log(data);
-      Array.from(data).forEach(cat => {
-        const img = document.createElement('img')
-        const catImageContainer = document.getElementById('catContainer')
-        img.src=cat.url
-        catImageContainer.append(img)
+        .then(res => res.json())
+        .then(data => {
+          console.log(data);
+          Array.from(data).forEach(cat => {
+          const img = document.createElement('img')
+          const catImageContainer = document.getElementById('catContainer')
+          img.src=cat.url
+          catImageContainer.append(img)
       })
     })}
   })
+  function listCatLink(catImageContainer)
+  catImageContainer.addEventListener('click')
+   (e); {
+   e.preventDefault()
+   const catImageContainer = e.Target.querySelector(catImageContainer)
+   function catImageContainer(renderCatImages) {
+   renderCatImages(CatImageContainer)
+   CatImageContainer.removeEventListener(click)
+   }
+   
+   }
   mainDiv().AppendChild(h1);
   mainDiv().AppendChild(p);
  
  
  resetMainDiv();
-  const h1 = document.CreateElement("h1");
-  const ul = document.CreateElement("ul");
-  const li = document.CreateElement("li");
+const h1 = document.CreateElement("h1");
+const ul = document.CreateElement("ul");
+const li = document.CreateElement("li");
   
 //DomContentLoaded//
 document.addEventListener("DomContentLoaded"), () => {
@@ -68,4 +81,3 @@ RenderListHomepage();
 AttachListCatClick();
 AttachListCatClick();
 AttachHomePageClickEvent();
-
